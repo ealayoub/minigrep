dbf228366b1c4484d38cb43ea7a5782fa978bc68
ealayoub <ssalayoub@gmail.com>
2023-04-08 16:33:32 +0300

    take in arguments and read file

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\minigrep.exe: C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\src\main.rs
