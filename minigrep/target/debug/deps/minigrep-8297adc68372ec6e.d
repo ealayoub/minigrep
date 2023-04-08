98fee6f8270d5451787ef880afc3ec9043ce258f
ealayoub <ssalayoub@gmail.com>
2023-04-08 16:09:48 +0300

    first commit

---
+++ minigrep/target/debug/deps/minigrep-8297adc68372ec6e.d
@@ -0,0 +1,5 @@
+c:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-8297adc68372ec6e.rmeta: src\main.rs
+
+c:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-8297adc68372ec6e.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/deps/minigrep-a695063dff7fbf04.d
@@ -0,0 +1,5 @@
+c:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-a695063dff7fbf04.rmeta: src\main.rs
+
+c:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-a695063dff7fbf04.d: src\main.rs
+
+src\main.rs:
