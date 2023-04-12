17bc84ccdff32c0ca32291cba43111484e708536
ealayoub <ssalayoub@gmail.com>
2023-04-12 11:59:41 +0300

    added search function and test module

---
+++ minigrep/target/debug/deps/minigrep-2bca44d93ad05ff6.d
@@ -0,0 +1,5 @@
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-2bca44d93ad05ff6.exe: src\main.rs
+
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-2bca44d93ad05ff6.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/deps/minigrep-2cfc2764bfbfa75f.d
@@ -0,0 +1,5 @@
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-2cfc2764bfbfa75f.exe: src\lib.rs
+
+C:\Users\essaa\desktop\rust-projects\minigrep\minigrep\target\debug\deps\minigrep-2cfc2764bfbfa75f.d: src\lib.rs
+
+src\lib.rs:
